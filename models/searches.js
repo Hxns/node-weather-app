359bc444364522a092dbb9433518ddbf5cc2e530
Herzfelder <dev.herzfelder>
2021-06-05 15:45:18 +0200

    searches - city

--- models/searches.js
+++ models/searches.js
@@ -19,4 +19,45 @@ class Searches {
             return words.join(' ')
         })
     }
+
+    get paramsMapbox() {
+        return {
+            'access_token': process.env.MAPBOX_KEY,
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+
+    get paramsWeather() {
+        return {
+            appid: process.env.OPENWEATHER_KEY,
+            units: 'metric',
+            lang: 'es'
+        }
+    }
+
+    async city( place = '' ) {
+
+        try {
+            // Petición http
+            const intance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ place }.json`,
+                params: this.paramsMapbox
+            });
+
+            const resp = await intance.get();
+            return resp.data.features.map( place => ({
+                id: place.id,
+                name: place.place_name,
+                lng: place.center[0],
+                lat: place.center[1],
+            }));
+            
+        } catch (error) {
+            return [];
+        }
+    }
+
+    
+
 }
